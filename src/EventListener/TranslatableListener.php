9d9639de93c3dcddb89c9451796bd1dbf4c6a9d1
Ondřej Dočkal <docky0@email.cz>
2019-08-21 10:52:51 +0200

    Change EntityManager to EntityManagerInterface

--- src/EventListener/TranslatableListener.php
+++ src/EventListener/TranslatableListener.php
@@ -2,7 +2,7 @@
 
 namespace Apploud\Doctrine\Translatable\EventListener;
 
-use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Metadata\MetadataFactory;
 use Prezent\Doctrine\Translatable\EventListener\TranslatableListener as PrezentListener;
 use Prezent\Doctrine\Translatable\Mapping\Driver\DoctrineAdapter;
@@ -10,7 +10,7 @@ use Prezent\Doctrine\Translatable\Mapping\Driver\DoctrineAdapter;
 class TranslatableListener extends PrezentListener
 {
 
-	public function __construct(EntityManager $entityManager)
+	public function __construct(EntityManagerInterface $entityManager)
 	{
 		$driver = DoctrineAdapter::fromManager($entityManager);
 		$metadataFactory  = new MetadataFactory($driver);
